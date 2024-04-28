b435cdf90401053fc71da93a23caa62337a770e8
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-04-28 12:31:18 +0530

    UI Changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Home.css'
 
 function Home(){
 
@@ -7,7 +9,8 @@ function Home(){
 
     useEffect(()=>{
         const fetchData = ()=>{
-            axios.get('https://eric-robotics-backend.vercel.app/')
+
+            axios.get('http://localhost:3001/')
             .then(res=>{
                 setData(res.data)
                 console.log(res)
@@ -17,20 +20,29 @@ function Home(){
         fetchData()
     },[])
 
+
     return(
-        <div>
-            <h1>Home</h1>
-            {
-                Data.map((Details)=>(
-                    <div>
-                        <p>{Details.id}</p>
-                        <p>{Details.Name}</p>
-                        <p>{Details.BatteryLevel}</p>
-                        <p>{Details.Status}</p>
-                        <p>{Details.RecentActivity}</p>
-                    </div>
-                ))
-            }
+        <div className='bg-primary bg-card'>
+            <h1 className='bg-light w-50 m-auto p-3'>ROBO Details</h1>
+
+            <div className='m-3'>
+                <select>
+                    <option value="Select">Select</option>
+                </select>
+            </div>
+            <div className='d-flex justify-content-center flex-wrap'>
+                {
+                    Data.map((Details)=>(
+                        <div className='bg-light p-3 m-5  rounded'>
+                            <p>Name : {Details.Name}</p>
+                            <p>Battery Level : {Details.BatteryLevel}</p>
+                            <p>Status : {Details.Status}</p>
+                            <p>RecentActivity : {Details.RecentActivity}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <button className='btn btn-warning m-3' onClick={e=>(window.location.reload())}>Refresh</button>
         </div>
     )
 }
